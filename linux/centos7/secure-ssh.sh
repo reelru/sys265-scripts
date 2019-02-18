3794ffeca6835b09a8efe54a49a0b42144047aaf
reelru <ella00rose@gmail.com>
2019-02-18 10:45:01 -0500

    Completed Secure-SSH Script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,4 +4,16 @@
 #Adds a public key to that users authorized in keys file
 #Removes root ability to ssh in
 
-echo "testing 123"
+#  Creates a user that logs in via RSA Private Key
+
+sudo useradd $1
+sudo mkdir /home/$1/.ssh
+sudo cp ../public-keys/sys265.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+
+#  Changes the sshd file to disallow root login
+
+sudo sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
