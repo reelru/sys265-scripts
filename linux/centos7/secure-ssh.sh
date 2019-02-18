1ab94d16de357a9b9a1ec7c287753bbf7dab76e2
reelru <ella00rose@gmail.com>
2019-02-18 10:27:50 -0500

    Organized Directories

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+#SECURE-SSH.SH
+#AUTHOR REELRU (ELLA)
+#Creates a new ssh user called $1
+#Adds a public key to that users authorized in keys file
+#Removes root ability to ssh in
+
+echo "testing 123"
